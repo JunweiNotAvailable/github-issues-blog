ab8ea8d446c66f61db78be14aa72677f37ac9823
Junwei <wertycxx@gmail.com>
2024-02-11 13:15:56 +0800

    fiexed auth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,5 @@
 import NextAuth from 'next-auth/next';
-import { AuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-
-export const authOptions:AuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.clientId as string,
-      clientSecret: process.env.clientSecret as string,
-      authorization: { params: { scope: 'read:user user:email repo' } },
-    }),
-  ],
-}
+import authOptions from '@/app/lib/options';
 
 const handler = NextAuth(authOptions);
 
---
+++ src/app/lib/options.ts
@@ -0,0 +1,14 @@
+import { AuthOptions } from "next-auth";
+import GithubProvider from 'next-auth/providers/github';
+
+const authOptions:AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.clientId as string,
+      clientSecret: process.env.clientSecret as string,
+      authorization: { params: { scope: 'read:user user:email repo' } },
+    }),
+  ],
+}
+
+export default authOptions;
